8dd2a97fa09a17ec9011cdf1d44ee43a21abe053
Joseph Hobbs <josephrosshobbs@gmail.com>
2024-12-09 16:37:52 -0500

    Improve modulus block

--- hdl/modulus.sv
+++ hdl/modulus.sv
@@ -15,7 +15,7 @@ module modulus #(
     );
 
     // Pipeline depth
-    localparam DEPTH = WIDTH + 1;
+    localparam DEPTH = WIDTH / 2;
 
     // Index width
     localparam INDEX_WIDTH = $clog2(DEPTH);
@@ -49,7 +49,7 @@ module modulus #(
             intermediate <= 0;
             subtrahend <= 0;
             state <= 1'b0;
-            index <= 2'b0;
+            index <= 0;
         end else begin
             case (state)
                 1'b0: begin
@@ -66,22 +66,70 @@ module modulus #(
                     // Perform subtraction operations
 
                     // Subtract the relevant multiple of the modulus, if necessary
-                    if (intermediate[index] >= (subtrahend << 1) + subtrahend) begin
-                        intermediate[index + 1] <= intermediate[index] - (subtrahend << 1) - subtrahend;
-                    end else if (intermediate[index] >= (subtrahend << 1)) begin
-                        intermediate[index + 1] <= intermediate[index] - (subtrahend << 1);
+                    if (intermediate[index] >= 15 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 15 * subtrahend;
+                    end else if (intermediate[index] >= 14 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 14 * subtrahend;
+                    end else if (intermediate[index] >= 13 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 13 * subtrahend;
+                    end else if (intermediate[index] >= 12 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 12 * subtrahend;
+                    end else if (intermediate[index] >= 11 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 11 * subtrahend;
+                    end else if (intermediate[index] >= 10 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 10 * subtrahend;
+                    end else if (intermediate[index] >= 9 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 9 * subtrahend;
+                    end else if (intermediate[index] >= 8 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 8 * subtrahend;
+                    end else if (intermediate[index] >= 7 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 7 * subtrahend;
+                    end else if (intermediate[index] >= 6 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 6 * subtrahend;
+                    end else if (intermediate[index] >= 5 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 5 * subtrahend;
+                    end else if (intermediate[index] >= 4 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 4 * subtrahend;
+                    end else if (intermediate[index] >= 3 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 3 * subtrahend;
+                    end else if (intermediate[index] >= 2 * subtrahend) begin
+                        intermediate[index + 1] <= intermediate[index] - 2 * subtrahend;
                     end else if (intermediate[index] >= subtrahend) begin
                         intermediate[index + 1] <= intermediate[index] - subtrahend;
                     end else begin
                         intermediate[index + 1] <= intermediate[index];
                     end
 
-                    if (index == DEPTH-1) begin
+                    if (index == DEPTH-1 || subtrahend < modulus_in) begin
                         // We're done
-                        if (intermediate[index] >= (modulus_in << 1) + modulus_in) begin
-                            value_out <= intermediate[index] - (modulus_in << 1) - modulus_in;
-                        end else if (intermediate[index] >= (modulus_in << 1)) begin
-                            value_out <= intermediate[index] - (modulus_in << 1);
+                        if (intermediate[index] >= 15 * modulus_in) begin
+                            value_out <= intermediate[index] - 15 * modulus_in;
+                        end else if (intermediate[index] >= 14 * modulus_in) begin
+                            value_out <= intermediate[index] - 14 * modulus_in;
+                        end else if (intermediate[index] >= 13 * modulus_in) begin
+                            value_out <= intermediate[index] - 13 * modulus_in;
+                        end else if (intermediate[index] >= 12 * modulus_in) begin
+                            value_out <= intermediate[index] - 12 * modulus_in;
+                        end else if (intermediate[index] >= 11 * modulus_in) begin
+                            value_out <= intermediate[index] - 11 * modulus_in;
+                        end else if (intermediate[index] >= 10 * modulus_in) begin
+                            value_out <= intermediate[index] - 10 * modulus_in;
+                        end else if (intermediate[index] >= 9 * modulus_in) begin
+                            value_out <= intermediate[index] - 9 * modulus_in;
+                        end else if (intermediate[index] >= 8 * modulus_in) begin
+                            value_out <= intermediate[index] - 8 * modulus_in;
+                        end else if (intermediate[index] >= 7 * modulus_in) begin
+                            value_out <= intermediate[index] - 7 * modulus_in;
+                        end else if (intermediate[index] >= 6 * modulus_in) begin
+                            value_out <= intermediate[index] - 6 * modulus_in;
+                        end else if (intermediate[index] >= 5 * modulus_in) begin
+                            value_out <= intermediate[index] - 5 * modulus_in;
+                        end else if (intermediate[index] >= 4 * modulus_in) begin
+                            value_out <= intermediate[index] - 4 * modulus_in;
+                        end else if (intermediate[index] >= 3 * modulus_in) begin
+                            value_out <= intermediate[index] - 3 * modulus_in;
+                        end else if (intermediate[index] >= 2 * modulus_in) begin
+                            value_out <= intermediate[index] - 2 * modulus_in;
                         end else if (intermediate[index] >= modulus_in) begin
                             value_out <= intermediate[index] - modulus_in;
                         end else begin
@@ -107,7 +155,7 @@ module modulus #(
                         index <= index + 1;
 
                         // Update the subtrahend
-                        subtrahend <= subtrahend >> 2;
+                        subtrahend <= subtrahend >> 4;
                     end
                 end
 
